1102eedc022402ac27a5714ec5330e3f7ef143e6
Lusuarezb <lusuarezb@unal.edu.co>
2022-11-25 22:52:53 -0500

    Intento 3

--- preguntas.py
+++ preguntas.py
@@ -129,7 +129,7 @@ def pregunta_04():
     # considerar 10 valores entre 0.1 y 1.0 para el parámetro alpha de
     # BernoulliNB.
     param_grid = {
-        "BernoulliNB": np.arange(0.1, 1.1, 0.1),
+        "BernoulliNB__alpha": np.arange(0.1, 1.1, 0.1),
     }
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
